dd29bf0b8d834c94fbeaa1136d5867e217a1b2d6
VaxyPoo <vapixedyt@gmail.com>
2024-08-05 17:35:15 -0700

    Create EnchanceScreenshare.plugin.js

---
+++ EnchanceScreenshare.plugin.js
@@ -0,0 +1,53 @@
+/**
+ * @name EnhanceScreenshare
+ * @description Sets screenshare quality to 1080p and 60 FPS.
+ * @version 1.0.0
+ */
+
+const config = {
+    info: {
+        name: "EnhanceScreenshare",
+        authors: [
+            {
+                name: "YourName",
+                discord_id: "YourDiscordID",
+                github_username: "YourGitHubUsername"
+            }
+        ],
+        version: "1.0.0",
+        description: "Sets screenshare quality to 1080p and 60 FPS."
+    }
+};
+
+module.exports = class EnhanceScreenshare {
+    constructor() {
+        this.settings = {};
+    }
+
+    start() {
+        this.overrideScreenshareSettings();
+    }
+
+    stop() {
+        // Optional: Clean up when the plugin is stopped
+    }
+
+    overrideScreenshareSettings() {
+        const originalSetVideoQuality = WebRTC.prototype.setVideoQuality;
+
+        WebRTC.prototype.setVideoQuality = function (videoQuality) {
+            console.log("EnhanceScreenshare: Overriding video quality settings");
+
+            videoQuality.width = 1920;
+            videoQuality.height = 1080;
+            videoQuality.frameRate = 60; // Setting to the highest practical FPS
+
+            return originalSetVideoQuality.apply(this, arguments);
+        };
+
+        // Apply immediately if WebRTC is already initialized
+        if (WebRTC.getInstance()) {
+            WebRTC.getInstance().setVideoQuality(WebRTC.getInstance().getVideoQuality());
+        }
+    }
+};
